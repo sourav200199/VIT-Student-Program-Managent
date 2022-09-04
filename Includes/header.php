009660d262747b3535a9efd777726c96c79c0669
Sourav Chakraborty <sourav.992001@gmail.com>
2022-09-04 17:04:54 +0530

    Updated

--- Includes/header.php
+++ Includes/header.php
@@ -13,7 +13,7 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="https://vtop.vit.ac.in/vtop/open/page">VIT University</a>
+      <a class="navbar-brand" href="https://vtop.vit.ac.in/vtop/open/page"><img src="CSS/vit.png" style="height: 35px; margin:2px"></a>
 
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
